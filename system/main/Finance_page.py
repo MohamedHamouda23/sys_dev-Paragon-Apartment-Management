452f306e0d230fa30380bb6642f59e44b277e009
MohamedHamouda23 <mohamedhamouda1267@yahoo.com>
2026-03-23 14:50:47 +0000

    test

---
+++ system/main/Finance_page.py
@@ -0,0 +1,5 @@
+# ID	Requirement Description	User Type / Role
+# FR3.3	Record and flag late payments; apply late fees or penalties and show graph of late payments per property.	Finance Manager
+# FR3.5	See payment history and download invoices.	Finance Manager / Tenants
+# FR6.10	Make payments; system checks card and records payment.	Finance Manager / Tenants
+# FR6.7	See own payment record and get alerts for late payments.	Finance Manager / Tenants
